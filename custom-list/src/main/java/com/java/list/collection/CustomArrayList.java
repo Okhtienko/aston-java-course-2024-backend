4a5e2863a35d22967e96ebed4663abf3b77b3e09
Mary Okhtienko <mariohtienko@gmail.com>
2024-07-16 11:03:02 +0300

    Update CustomArrayListTest.java

--- custom-list/src/main/java/com/java/list/collection/CustomArrayList.java
+++ custom-list/src/main/java/com/java/list/collection/CustomArrayList.java
@@ -1,11 +1,13 @@
 package com.java.list.collection;
 
-import com.java.list.utils.CustomList;
+import com.java.list.structure.CustomList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.function.Consumer;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -24,7 +26,7 @@ public class CustomArrayList<E> implements CustomList<E>, Iterable<E>  {
     public CustomArrayList(int capacity) {
         size = 0;
         if (capacity < 0) {
-            throw new IllegalArgumentException("Illegal Capacity: " + capacity);
+            throw new IllegalArgumentException();
         }
         elements = (E[]) (capacity == 0 ? EMPTY : new Object[capacity]);
     }
@@ -87,9 +89,7 @@ public class CustomArrayList<E> implements CustomList<E>, Iterable<E>  {
 
     @Override
     public void sort(Comparator<? super E> comparator) {
-        if (comparator == null) {
-            throw new NullPointerException("Comparator can not be null");
-        }
+        Objects.requireNonNull(comparator);
         Arrays.sort(elements, 0, size, comparator);
     }
 
@@ -123,6 +123,9 @@ public class CustomArrayList<E> implements CustomList<E>, Iterable<E>  {
 
             @Override
             public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
                 return elements[currentIndex++];
             }
 
@@ -140,6 +143,8 @@ public class CustomArrayList<E> implements CustomList<E>, Iterable<E>  {
 
     @Override
     public void forEach(Consumer<? super E> action) {
+        Objects.requireNonNull(action);
+
         for (E element : this) {
             action.accept(element);
         }
@@ -164,7 +169,7 @@ public class CustomArrayList<E> implements CustomList<E>, Iterable<E>  {
 
     private void check(int index) {
         if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+            throw new IndexOutOfBoundsException();
         }
     }
 
